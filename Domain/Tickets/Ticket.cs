e3218d9e1beb034e82b6008d419e46ea8cfba6d0
CathyAkoth <catherinececilia22@gmail.com>
2021-10-21 21:12:55 -0700

    Adding entities to ticket

--- Domain/Tickets/Ticket.cs
+++ Domain/Tickets/Ticket.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using Domain.Comments;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
-public class Class1
+
+namespace Domain.Tickets
 {
-	public Class1()
-	{
-	}
-}
+    [BsonIgnoreExtraElements]
+    public class Ticket
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Summary { get; set; }
+        public int TicketNumber { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public Status Status { get; set; }
